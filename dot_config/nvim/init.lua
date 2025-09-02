7f3bdbdc996b9154a43379e9034773e1a2584963
josephwest2 <joeywest1414@gmail.com>
2025-09-02 15:02:34 -0500

    added plugins to nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -27,4 +27,34 @@ vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 vim.keymap.set("t", "<C-[>", [[<C-\><C-n>]])
 vim.keymap.set("t", "C-c", [[<C-\><C-n>]])
 
+-- Lazy Setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+    { 'm4xshen/autoclose.nvim' },
+    { 'nvim-treesitter/nvim-treesitter', lazy = false },
+    { 'Pocco81/auto-save.nvim' },
+    { 'navarasu/onedark.nvim' },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "onedark" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
