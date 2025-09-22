b35f82d81d5a7b4cac3b1e99bac776b9591f4504
Joseph West <joeywest1414@gmail.com>
2025-09-21 21:31:17 -0500

    altered codeium keybinds

--- dot_config/nvim/lua/joey/plugins/codeium.lua
+++ dot_config/nvim/lua/joey/plugins/codeium.lua
@@ -4,13 +4,13 @@ return {
     config = function()
         vim.g.codeium_disable_bindings = true
         vim.keymap.set('i', '<C-j>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-        vim.keymap.set('i', '<C-k>', function() return vim.fn['codeium#AcceptNextWord']() end,
+        vim.keymap.set('i', '<C-h>', function() return vim.fn['codeium#AcceptNextWord']() end,
             { expr = true, silent = true })
         vim.keymap.set('i', '<C-l>', function() return vim.fn['codeium#AcceptNextLine']() end,
             { expr = true, silent = true })
-        vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+        vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
             { expr = true, silent = true })
-        vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
             { expr = true, silent = true })
     end
 }
