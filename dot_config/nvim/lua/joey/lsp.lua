aca034c94d4f642c4bf1cbcef84f4e19a54545db
Joseph West <joeywest1414@gmail.com>
2025-09-27 11:10:30 -0500

    added gopls

--- dot_config/nvim/lua/joey/lsp.lua
+++ dot_config/nvim/lua/joey/lsp.lua
@@ -5,6 +5,7 @@ local prefix = "joey.lsp."
 function M.init()
     require(prefix .. "clangd").init()
     require(prefix .. "lua_ls").init()
+    require(prefix .. "gopls").init()
 
     vim.diagnostic.config({
         virtual_lines = {
