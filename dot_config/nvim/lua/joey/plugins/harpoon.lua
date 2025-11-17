d4ecb6aa2d3c22059b17f9ac03cc1a745927ce9f
Joseph West <joeywest1414@gmail.com>
2025-11-17 00:21:29 -0600

    eddited harpoon keybinds

--- dot_config/nvim/lua/joey/plugins/harpoon.lua
+++ dot_config/nvim/lua/joey/plugins/harpoon.lua
@@ -6,7 +6,7 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
         vim.keymap.set("n", "<leader>hr", function() harpoon:list():remove() end)
 
