0eb3d0034c8af38d3107a45029c47a728449243c
Joseph West <joeywest1414@gmail.com>
2026-01-10 16:39:02 -0600

    moved qt6ct env var from fish to hyprland

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     bind \cy 'commandline -f accept-autosuggestion'
     export EDITOR=nvim
     export SDL_VIDEODRIVER=wayland
-    export QT_QPA_PLATFORMTHEME=qt6ct
 end
 
 # bun
