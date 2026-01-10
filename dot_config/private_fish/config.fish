5c5bf3314e3aa961f0feb632c4e2c1607f60ff88
Joseph West <joeywest1414@gmail.com>
2026-01-10 16:32:51 -0600

    added qt6ct to fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     bind \cy 'commandline -f accept-autosuggestion'
     export EDITOR=nvim
     export SDL_VIDEODRIVER=wayland
+    export QT_QPA_PLATFORMTHEME=qt6ct
 end
 
 # bun
