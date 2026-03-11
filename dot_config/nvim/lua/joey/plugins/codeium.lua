76d2cb3873af992f7f88b504bb071fb98fa837f2
Joseph West <joeywest1414@gmail.com>
2026-03-11 01:02:47 -0500

    altered codeium keybinds

--- dot_config/nvim/lua/joey/plugins/codeium.lua
+++ dot_config/nvim/lua/joey/plugins/codeium.lua
@@ -13,11 +13,11 @@ return {
             { expr = true, silent = true })
         vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
             { expr = true, silent = true })
-        vim.keymap.set({'n', 'i'}, '<leader>ae', function()
+        vim.keymap.set('n', '<leader>ae', function()
             vim.cmd('Codeium Enable')
             print('autosuggestions enabled')
         end)
-        vim.keymap.set({'n', 'i'}, '<leader>ad', function()
+        vim.keymap.set('n', '<leader>ad', function()
             vim.cmd('Codeium Disable')
             print('autosuggestions disabled')
         end)
