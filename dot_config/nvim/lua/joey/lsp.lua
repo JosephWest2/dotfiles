dc04a2801f3c1a17353fd5ae408aa66b4e9d5d6e
Joseph West <joeywest1414@gmail.com>
2026-05-15 21:42:00 -0500

    added mason-lspconfig, disabled opencode

--- dot_config/nvim/lua/joey/lsp.lua
+++ dot_config/nvim/lua/joey/lsp.lua
@@ -1,17 +1,6 @@
 local M = {}
 
 function M.init()
-    vim.lsp.enable('clangd')
-    vim.lsp.enable('lua_ls')
-    vim.lsp.enable('gopls')
-    vim.lsp.enable('rust_analyzer')
-    vim.lsp.enable('zls')
-    vim.lsp.enable('templ')
-    vim.lsp.enable('svelte')
-    vim.lsp.enable('vtsls')
-    vim.lsp.enable('jdtls')
-    vim.lsp.enable('pyright')
-
     vim.g.zig_fmt_autosave = 0
 
     vim.diagnostic.config({
--- dot_config/nvim/lua/joey/plugins/lspconfig.lua
+++ dot_config/nvim/lua/joey/plugins/lspconfig.lua
@@ -2,6 +2,8 @@ return {
     'neovim/nvim-lspconfig',
     lazy = false,
     config = function()
-        local capabilities = require('blink.cmp').get_lsp_capabilities()
+        vim.lsp.config("*", {
+            capabilities = require('blink.cmp').get_lsp_capabilities()
+        })
     end
 }
